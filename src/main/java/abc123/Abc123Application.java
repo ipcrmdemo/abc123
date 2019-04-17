2f8a3aa3e4142b06c759eb083675e15a8f678673
Matt Cadorette <mcadorette@gmail.com>
2019-04-17 17:43:07 -0400

    Update Abc123Application.java

--- src/main/java/abc123/Abc123Application.java
+++ src/main/java/abc123/Abc123Application.java
@@ -9,4 +9,18 @@ public class Abc123Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Abc123Application.class, args);
 	}
+	
+	public void test() {
+
+        String[] field = {"a", "b", "c", "s", "e"};
+
+        //concatenates strings using + in a loop
+        String s = "";
+        for (int i = 0; i < field.length; ++i) {
+            s = s + field[i];
+        }
+
+        System.out.println(ip);
+
+    }
 }
